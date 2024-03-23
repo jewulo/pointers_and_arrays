27399b52f6d10611bce1a69dbb885e68446132e7
Joshua Ewulo <jewulo@gmail.com>
2024-03-23 22:28:23 +0000

    Add project files.

---
+++ pointers_and_arrays/pointers_and_arrays.cpp
@@ -0,0 +1,23 @@
+// pointers_and_arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// https://www.youtube.com/watch?v=4Rll-_e9-0M&list=PLdo5W4Nhv31bbKJzrsKfMpo_grxuLl8LU&index=4
+// https://www.youtube.com/watch?v=KDQXUysHLL8&list=PLdo5W4Nhv31bbKJzrsKfMpo_grxuLl8LU&index=5
+// https://www.youtube.com/watch?v=4Rll-_e9-0M&list=PLdo5W4Nhv31bbKJzrsKfMpo_grxuLl8LU&index=6
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
